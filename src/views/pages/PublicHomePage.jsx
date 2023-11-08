73e8508c3cc9c0ce50ba28321694aa3472bc0d88
imran122 <imransardar122@gmail.com>
2023-11-08 21:52:00 +0600

    config tailwind css

---
+++ src/views/pages/PublicHomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PublicHomePage = () => {
+    return (
+        <div>
+           <p className='bg-red-600 uppercase font-bold'>home</p> 
+           <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+        </div>
+    );
+};
+
+export default PublicHomePage;
